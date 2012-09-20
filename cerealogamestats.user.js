f2311ebdb45a687ec40174c5d82e0ca4609266e3
EliasGrande <elias.grande.casedas@gmail.com>
2012-09-20 04:49:56 +0200

    [Added] French locale (thanks to Elvara).

--- cerealogamestats.user.js
+++ cerealogamestats.user.js
@@ -5,7 +5,7 @@
 // @downloadURL    https://userscripts.org/scripts/source/134405.user.js
 // @updateURL      https://userscripts.org/scripts/source/134405.meta.js
 // @icon           http://s3.amazonaws.com/uso_ss/icon/134405/large.png
-// @version        2.0.1
+// @version        2.0.2
 // @include        *://*.ogame.*/game/index.php?*page=alliance*
 // ==/UserScript==
 /*!
@@ -403,6 +403,77 @@ if (/es|ar|mx/.test(ogameInfo.language))i18n.set(
 	o_abt:'Estadísticas realizadas con {link}',
 	// OGame Error
 	e_oga:'Error de OGame, recargar esta página puede arreglarlo'
+})
+
+// locale [fr] francais
+//
+// thanks to Elvara
+// http://userscripts.org/topics/116649
+
+else if (/fr/.test(ogameInfo.language))i18n.set(
+{
+	// separators
+	s_dec: ".",
+	s_tho: ",",
+	s_dat: "/",
+	s_tim: ":",
+	// abb time units
+	a_wee: "s",
+	a_day: "j",
+	a_hou: "h",
+	a_min: "m",
+	a_sec: "s",
+	// buttons
+	b_sel:'Sélectionner',
+	b_del:'Effacer',
+	b_get:'Recharger de cette page',
+	b_sav:'Sauvegarder comme anciennes données"',
+	b_loa:'Charger anciennes données',
+	// titles
+	t_odt:'Anciennes données',
+	t_ndt:'Nouvelles données',
+	t_fmt:'Format',
+	t_col:'Couleur',
+	t_inc:'Inclure',
+	t_out:'Statistiques (code)',
+	t_stb:'Statut',
+	t_pre:'Aperçu (en utilisant l’arrière plan foncé)',
+	// period
+	p_ago:'{period} depuis le début',
+	p_now:'maintenant',
+	// colors
+	c_dbg:'Arrière plan foncé',
+	c_lbg:'Arrière plan clair',
+	// status (errors)
+	e_nod:'Pas d\'anciennes données',
+	e_nnd:'Pas de nouvelles données',
+	e_odf:'Les anciennes données ont un mauvais format',
+	e_ndf:'Les nouvelles données ont un mauvais format',
+	e_unk:'Erreur inattendu',
+	e_ndt:'Pas de données',
+	e_wft:'Mauvais format',
+	// status (success)
+	w_dne:'Prêt',
+	w_pcs:'Traitement en cours',
+	// output
+	o_tdt:'Évolution de l\'alliance depuis {oldDate} à {newDate}',
+	o_tet:'Temps passé',
+	o_tas:'Résumé de l\'Alliance ',
+	o_ptl:'Points Totaux',
+	o_ppm:'Points par membres',
+	o_tts:'Top 3 par score',
+	o_ttp:'Top 3 par pourcentage',
+	o_trs:'Rang par Score',
+	o_trp:'Rang par Pourcentage',
+	o_tsc:'Cas spéciaux',
+	o_cnm:'Nouveaux Membres',
+	o_cla:'A quitter l\'alliance',
+	o_bdg:'Banni',
+	o_bdq:'Débanni',
+	o_ldt:'Dernières données (pour statistiques futures)',
+	o_abt:'Statistiques obtenues avec {link}',
+	// OGame Error
+	e_oga:'Erreur OGame, recharger la page peut régler le problème'
 });
 
 // colors
