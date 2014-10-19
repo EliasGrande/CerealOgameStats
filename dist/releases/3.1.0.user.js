6de33a29e67aa4c2fcd7bd204bf2980ced0be6a8
Elías Grande Cásedas <elias.grande.casedas@gmail.com>
2014-10-19 20:57:02 +0200

    Add icon

--- dist/releases/3.1.0.user.js
+++ dist/releases/3.1.0.user.js
@@ -4,6 +4,7 @@
 // @namespace    https://github.com/EliasGrande/
 // @downloadURL  https://github.com/EliasGrande/CerealOgameStats/raw/master/dist/releases/latest.user.js
 // @updateURL    https://github.com/EliasGrande/CerealOgameStats/raw/master/dist/releases/latest.meta.js
+// @icon         https://github.com/EliasGrande/CerealOgameStats/raw/master/dist/img/icon.png
 // @version      3.1.0
 // @include      *://*.ogame.*/game/index.php?*page=alliance*
 // ==/UserScript==
