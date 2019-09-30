e493a6402055683485f5070cb96d5c7586509bdf
chilly2go <2142902+chilly2go@users.noreply.github.com>
2019-09-30 10:58:44 +0200

    Create de_DE.js
    added german translation (except for banned / unbanned)

---
+++ dist/locale/de_DE.js
@@ -0,0 +1,68 @@
+{	
+	// separators
+	s_dec: ",",
+	s_tho: ".",
+	s_dat: ".",
+	s_tim: ":",
+	// abb time units
+	a_wee: "w",
+	a_day: "d",
+	a_hou: "h",
+	a_min: "m",
+	a_sec: "s",
+	// buttons
+	b_sel:'Auswählen',
+	b_del:'Entfernen',
+	b_get:'Von dieser Seite holen',
+	b_sav:'Als "Alte Daten" speichern',
+	b_loa:'Lade gespeicherte Daten',
+	b_res:'Statistiken zurücksetzen',
+	// titles
+	t_odt:'Alte Daten',
+	t_ndt:'Neue Daten',
+	t_fmt:'Format',
+	t_col:'Farben',
+	t_inc:'Einbeziehen',
+	t_out:'Statistiken (code)',
+	t_stb:'Status',
+	t_pre:'Veränderung',
+	t_exp:'Export für Foren',
+	// period
+	p_ago:'vor {period}',
+	p_now:'jetzt',
+	// colors
+	c_dbg:'Dunkler Hintergrund',
+	c_lbg:'Heller Hintergrund',
+	// status (errors)
+	e_nod:'Keine alten Daten',
+	e_nnd:'Keine neuen Daten',
+	e_odf:'Die alten Daten haben ein falsches Format',
+	e_ndf:'Die neuen Daten haben ein falsches Format',
+	e_unk:'Unerwarteter Fehler',
+	e_ndt:'Keine Daten',
+	e_wft:'Falsches Format',
+	// status (success)
+	w_pcs:'Verarbeitung',
+	// output
+	o_tdt:'Änderungen in der Allianz zwischen {oldDate} und {newDate}',
+	o_tet:'Vergangene Zeit',
+	o_tas:'Allianz Zusammenfassung',
+	o_ptl:'Gesammte Punkte',
+	o_ppm:'Punkte je Mitglied',
+	o_ttt:'Top 3 nach Gesamtpunkten',
+	o_tts:'Top 3 nach gewonnenen Punkten',
+	o_ttp:'Top 3 nach gewonnenen Prozenten',
+	o_ttg:'Top 3 nach gewonnenen Positionen',
+	o_trt:'Gesamt Punkteränge',
+	o_trs:'Gewonnene Punkterang',
+	o_trp:'Gewonnene Prozentrang',
+	o_trg:'Gewonnene Positionsrang',
+	o_tsc:'Spezial Fälle',
+	o_cnm:'Neue Mitglieder',
+	o_cla:'Verlässt die Allianz',
+	o_bdg:'banned',
+	o_bdq:'unbanned',
+	o_abt:'Statistiken erstellt mit {link}',
+	// OGame Error
+	e_oga:'OGame Fehler, Neuladen der Seite könnte diesen Fehler beheben'
+}
